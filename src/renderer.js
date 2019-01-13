157533381597c49ab76994524341da72049f55e7
Torralba, Andres Jr R <andres.jr.r.torralba@accenture.com>
2019-01-14 01:13:44 +0800

    Datatables avoid reload. Hide only.

--- src/renderer.js
+++ src/renderer.js
@@ -1,11 +1,21 @@
 var app = require('./app');
 var currentUser = {};
+var initMain = true;
+var initAuditTrail = true;
+var tableAuditTrail;
 
 app.on('rendered', function (rendered) {
     $('#main').html(rendered);
 });
 
 $(document).ready(function () {
+    // $('#data-table-audit_wrapper').hide();
+    // $('#data-table-audit').hide();
+    // $('#data-table_wrapper').hide();
+    $('#container-interface').hide();
+    $('#container-audit').hide();
+
+    app.emit('view-selected', 'main');
 
     $.get('http://localhost:3000/user', function (data) {
         if (data.success) {
@@ -87,8 +97,12 @@ $(document).ready(function () {
 });
 
 function loadAuditTrail() {
-    app.emit('view-selected', 'audittrail');
-    $('#user-info-p').append(currentUser.FULLNAME);
+    // app.emit('view-selected', 'audittrail');
+    // $('#user-info-p').append(currentUser.FULLNAME);
+    // $('#data-table-audit_wrapper').show();
+    // $('#data-table_wrapper').hide();
+    $('#container-audit').show();
+    $('#container-interface').hide();
 
     var columns = [
         { 'data': 'TimeStamp' },
@@ -100,34 +114,41 @@ function loadAuditTrail() {
         { 'data': 'Comments' }
     ];
 
-    var table = $('#data-table-audit').DataTable({
-        order: [[0, 'desc']],
-        dom: 'Bfrtip',
-        buttons: {
-            buttons: [
-                {
-                    text: 'Back to Main Page',
-                    action: function (e, dt, node, config) {
-                        loadMain();
-                    }
-                },
-                { extend: 'copyHtml5', className: 'btn' },
-                { extend: 'csvHtml5', className: 'btn' }
-            ]
-        },
-        'ajax': 'http://localhost:3000/audit',
-        'columns': columns,
-        'drawCallback': function (settings) {
-            $('#spinner').hide();
-        }
-    });
+    if (initAuditTrail) {
+
+        initAuditTrail = false
+
+        tableAuditTrail = $('#data-table-audit').DataTable({
+            order: [[0, 'desc']],
+            dom: 'Bfrtip',
+            buttons: {
+                buttons: [
+                    {
+                        text: 'Back to Main Page',
+                        action: function (e, dt, node, config) {
+                            loadMain();
+                        }
+                    },
+                    { extend: 'copyHtml5', className: 'btn' },
+                    { extend: 'csvHtml5', className: 'btn' }
+                ]
+            },
+            'ajax': 'http://localhost:3000/audit',
+            'columns': columns,
+            'drawCallback': function (settings) {
+                $('#spinner').hide();
+            }
+        });
+    } else {
+        tableAuditTrail.ajax.reload();
+    }
 
 }
 
 function loadMain() {
-    app.emit('view-selected', 'main');
-    $('#user-info').append(currentUser.FULLNAME);
-
+    $('#container-audit').hide();
+    $('#container-interface').show();
+    
     var columns = [
         { 'data': 'ID' },
         { 'data': 'InterfaceId' },
@@ -143,160 +164,116 @@ function loadMain() {
         { 'data': 'Notation' },
         { 'data': 'OtherInfo' }
     ];
-
+    
     var selectedRecords = [];
-
-    var table = $('#data-table').DataTable({
-        dom: 'Bfrtip',
-        buttons: {
-            buttons: [
-                {
-                    text: 'Add',
-                    action: function (e, dt, node, config) {
-                        $('#dlg-add-record').modal('show');
-                    }
-                },
-                {
-                    text: 'Update Selected Row(s)',
-                    action: function (e, dt, node, config) {
-                        if (selectedRecords === undefined || selectedRecords.length == 0) {
-                            alert('Please select a record.');
-                        } else {
-                            $('#dlg-update-sel-rows').modal('show');
+    
+    if (initMain) {
+        $('#user-info').append(currentUser.FULLNAME);
+        initMain = false;
+        var table = $('#data-table').DataTable({
+            dom: 'Bfrtip',
+            buttons: {
+                buttons: [
+                    {
+                        text: 'Add',
+                        action: function (e, dt, node, config) {
+                            $('#dlg-add-record').modal('show');
                         }
-                    }
-                },
-                {
-                    text: 'Audit Trail',
-                    action: function (e, dt, node, config) {
-                        loadAuditTrail();
-                    }
-                },
-                { extend: 'copyHtml5', className: 'btn' },
-                { extend: 'csvHtml5', className: 'btn' }
-            ]
-        },
-        select: 'multi',
-        'ajax': 'http://localhost:3000/data',
-        'columns': columns,
-        'drawCallback': function (settings) {
-            $('#spinner').hide();
-        }
-    });
-
-    table
-        .on('select.dt', function (e, dt, type, indexes) {
-            selectedRecords.push(table.rows(indexes).data().toArray()[0]);
-        })
-        .on('deselect.dt', function (e, dt, type, indexes) {
-            selectedRecords.pop(table.rows(indexes).data().toArray()[0])
-        })
-        .on('error.dt', function (e, settings, techNote, message) {
-            alert('ERROR: ' + JSON.stringify(message));
-        })
-        .on('dblclick', 'tr', function () {
-            var data = table.row(this).data();
-            setFields(data);
-            $('#dlg-add-record').modal('show');
+                    },
+                    {
+                        text: 'Update Selected Row(s)',
+                        action: function (e, dt, node, config) {
+                            if (selectedRecords === undefined || selectedRecords.length == 0) {
+                                alert('Please select a record.');
+                            } else {
+                                $('#dlg-update-sel-rows').modal('show');
+                            }
+                        }
+                    },
+                    {
+                        text: 'Audit Trail',
+                        action: function (e, dt, node, config) {
+                            loadAuditTrail();
+                        }
+                    },
+                    { extend: 'copyHtml5', className: 'btn' },
+                    { extend: 'csvHtml5', className: 'btn' }
+                ]
+            },
+            select: 'multi',
+            'ajax': 'http://localhost:3000/data',
+            'columns': columns,
+            'drawCallback': function (settings) {
+                $('#spinner').hide();
+            }
         });
 
-    // Populate Options in 'Field' dropdown.
-    $.each(columns.slice(1), function (key, value) {
-        $('#fld-field').prepend('<option value="' + value.data + '">' + value.data + '</option>');
-    });
-
-    // Click event for Update Button
-    $('#btn-update').bind('click', function (e) {
-
-        if ($('#fld-newvalue').val().trim() === '') {
-            $('#fld-newvalue-help').removeClass('d-none');
-            e.preventDefault();
-            return;
-        } else {
-            if (!$('#fld-newvalue-help').hasClass('d-none')) {
-                $('#fld-newvalue-help').addClass('d-none');
-            }
-        }
-
-        if ($('#fld-comments').val().trim() === '') {
-            $('#fld-comments-help').removeClass('d-none');
-            e.preventDefault();
-            return;
-        } else {
-            if (!$('#fld-comments-help').hasClass('d-none')) {
-                $('#fld-comments-help').addClass('d-none');
-            }
-        }
-
-        $('#spinner').show();
-        var formData = {
-            field: '',
-            value: '',
-            comments: '',
-            rows: []
-        };
-
-        formData.field = $('#fld-field').val();
-        formData.value = $('#fld-newvalue').val();
-        formData.comments = $('#fld-comments').val();
-        formData.rows = selectedRecords;
-
-        $.post('http://localhost:3000/data', formData)
-            .done(function (data) {
-                if (data.success) {
-                    table.rows().deselect();
-                    selectedRecords = [];
-                    table.ajax.reload();
-                    $('#dlg-update-sel-rows').modal('hide');
-                    $('#fld-newvalue').val('');
-                    $('#fld-comments').val('');
-                } else {
-                    $('#spinner').hide();
-                    alert('ERROR: ' + JSON.stringify(data));
-                }
+        table
+            .on('select.dt', function (e, dt, type, indexes) {
+                selectedRecords.push(table.rows(indexes).data().toArray()[0]);
             })
-            .fail(function (xhr, status, error) {
-                alert('ERROR: ' + JSON.stringify(error));
+            .on('deselect.dt', function (e, dt, type, indexes) {
+                selectedRecords.pop(table.rows(indexes).data().toArray()[0])
+            })
+            .on('error.dt', function (e, settings, techNote, message) {
+                alert('ERROR: ' + JSON.stringify(message));
+            })
+            .on('dblclick', 'tr', function () {
+                var data = table.row(this).data();
+                setFields(data);
+                $('#dlg-add-record').modal('show');
             });
-    });
 
-    $('#btn-save').bind('click', function (e) {
+        // Populate Options in 'Field' dropdown.
+        $.each(columns.slice(1), function (key, value) {
+            $('#fld-field').prepend('<option value="' + value.data + '">' + value.data + '</option>');
+        });
 
-        if ($('#fld-interface-id').val().trim() === '') {
-            $('#fld-interface-id-help').removeClass('d-none');
-            e.preventDefault();
-            return;
-        } else {
-            if (!$('#fld-interface-id-help').hasClass('d-none')) {
-                $('#fld-interface-id-help').addClass('d-none');
+        // Click event for Update Button
+        $('#btn-update').bind('click', function (e) {
+
+            if ($('#fld-newvalue').val().trim() === '') {
+                $('#fld-newvalue-help').removeClass('d-none');
+                e.preventDefault();
+                return;
+            } else {
+                if (!$('#fld-newvalue-help').hasClass('d-none')) {
+                    $('#fld-newvalue-help').addClass('d-none');
+                }
             }
-        }
 
-        $('#spinner').show();
+            if ($('#fld-comments').val().trim() === '') {
+                $('#fld-comments-help').removeClass('d-none');
+                e.preventDefault();
+                return;
+            } else {
+                if (!$('#fld-comments-help').hasClass('d-none')) {
+                    $('#fld-comments-help').addClass('d-none');
+                }
+            }
 
-        var formData = {
-            id: $('#fld-id').val(),
-            interfaceId: $('#fld-interface-id').val(),
-            tableName: $('#fld-table-name').val(),
-            tableDescription: $('#fld-table-desc').val(),
-            subType: $('#fld-subtype').val(),
-            fieldName: $('#fld-fld-name').val(),
-            fieldDescription: $('#fld-fld-desc').val(),
-            dataType: $('#fld-data-type').val(),
-            fieldLength: $('#fld-length').val(),
-            outputType: $('#fld-output-type').val(),
-            outputLength: $('#fld-output-length').val(),
-            notation: $('#fld-notation').val(),
-            otherInfo: $('#fld-other-info').val()
-        };
+            $('#spinner').show();
+            var formData = {
+                field: '',
+                value: '',
+                comments: '',
+                rows: []
+            };
 
-        if (formData.id === '') {
-            $.post('http://localhost:3000/data/new', formData)
+            formData.field = $('#fld-field').val();
+            formData.value = $('#fld-newvalue').val();
+            formData.comments = $('#fld-comments').val();
+            formData.rows = selectedRecords;
+
+            $.post('http://localhost:3000/data', formData)
                 .done(function (data) {
                     if (data.success) {
+                        table.rows().deselect();
+                        selectedRecords = [];
                         table.ajax.reload();
-                        $('#dlg-add-record').modal('hide');
-                        clearFields();
+                        $('#dlg-update-sel-rows').modal('hide');
+                        $('#fld-newvalue').val('');
+                        $('#fld-comments').val('');
                     } else {
                         $('#spinner').hide();
                         alert('ERROR: ' + JSON.stringify(data));
@@ -305,24 +282,73 @@ function loadMain() {
                 .fail(function (xhr, status, error) {
                     alert('ERROR: ' + JSON.stringify(error));
                 });
-        } else {
-            $.post('http://localhost:3000/data/' + formData.id, formData)
-                .done(function (data) {
-                    if (data.success) {
-                        table.ajax.reload();
-                        $('#dlg-add-record').modal('hide');
-                        clearFields();
-                    } else {
-                        $('#spinner').hide();
-                        alert('ERROR: ' + JSON.stringify(data));
-                    }
-                })
-                .fail(function (xhr, status, error) {
-                    alert('ERROR: ' + JSON.stringify(error));
-                });
-        }
+        });
 
-    });
+        $('#btn-save').bind('click', function (e) {
+
+            if ($('#fld-interface-id').val().trim() === '') {
+                $('#fld-interface-id-help').removeClass('d-none');
+                e.preventDefault();
+                return;
+            } else {
+                if (!$('#fld-interface-id-help').hasClass('d-none')) {
+                    $('#fld-interface-id-help').addClass('d-none');
+                }
+            }
+
+            $('#spinner').show();
+
+            var formData = {
+                id: $('#fld-id').val(),
+                interfaceId: $('#fld-interface-id').val(),
+                tableName: $('#fld-table-name').val(),
+                tableDescription: $('#fld-table-desc').val(),
+                subType: $('#fld-subtype').val(),
+                fieldName: $('#fld-fld-name').val(),
+                fieldDescription: $('#fld-fld-desc').val(),
+                dataType: $('#fld-data-type').val(),
+                fieldLength: $('#fld-length').val(),
+                outputType: $('#fld-output-type').val(),
+                outputLength: $('#fld-output-length').val(),
+                notation: $('#fld-notation').val(),
+                otherInfo: $('#fld-other-info').val()
+            };
+
+            if (formData.id === '') {
+                $.post('http://localhost:3000/data/new', formData)
+                    .done(function (data) {
+                        if (data.success) {
+                            table.ajax.reload();
+                            $('#dlg-add-record').modal('hide');
+                            clearFields();
+                        } else {
+                            $('#spinner').hide();
+                            alert('ERROR: ' + JSON.stringify(data));
+                        }
+                    })
+                    .fail(function (xhr, status, error) {
+                        alert('ERROR: ' + JSON.stringify(error));
+                    });
+            } else {
+                $.post('http://localhost:3000/data/' + formData.id, formData)
+                    .done(function (data) {
+                        if (data.success) {
+                            table.ajax.reload();
+                            $('#dlg-add-record').modal('hide');
+                            clearFields();
+                        } else {
+                            $('#spinner').hide();
+                            alert('ERROR: ' + JSON.stringify(data));
+                        }
+                    })
+                    .fail(function (xhr, status, error) {
+                        alert('ERROR: ' + JSON.stringify(error));
+                    });
+            }
+
+        });
+
+    }
 }
 
 function setFields(data) {
--- src/server/db.config.js
+++ src/server/db.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: 'datahub.mdb',
-    path: 'C:\\Users\\andres.jr.r.torralba\\Documents\\',
+    // path: 'C:\\Users\\andres.jr.r.torralba\\Documents\\',
+    path: 'C:\\',
     password: 'btbadmin',
     provider: 'Microsoft.Jet.OLEDB.4.0'
 }
